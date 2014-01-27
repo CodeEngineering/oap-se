58f559938d3e095798fa3616669533e45b298ba9
badasscoder <florante.kho@gmail.com>
2014-01-28 06:59:08 +0800

    remove smtp

--- application/config/email.php
+++ application/config/email.php
@@ -14,11 +14,6 @@ $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
 $config['newline'] = "\r\n";
 
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'ssl://smtp.googlemail.com';
-$config['smtp_port'] = 465;
-$config['smtp_user'] = 'florante.kho@gmail.com';
-$config['smtp_pass'] = '13$Rfv5tgb';
 
 
 /* End of file email.php */
