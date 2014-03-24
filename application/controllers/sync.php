4884626b664d1a47f6c555909deaed735a3960d6
badasscoder <florante.kho@gmail.com>
2014-03-24 16:46:11 +0800

    Update sync.php

--- application/controllers/sync.php
+++ application/controllers/sync.php
@@ -73,17 +73,17 @@ function runnow()
 	$total_found=count($res);
 	
 	$tmp=$this->apis[$connection->api_target]->obj_name;
-	exit;
+	
 	$api_target=new $tmp();
 	//echo 'point::'.$this->apis[$connection->api_target]->write_point;exit;
-	
-	$sync_log=$api_target->Create($this->apis[$connection->api_target]->write_point,$res,json_decode($connection->target_fields));
+	//exit;
+//	$sync_log=$api_target->Create($this->apis[$connection->api_target]->write_point,$res,json_decode($connection->target_fields));
 	
 	$targetpoint=$api_target->api['users'];
-	echo 'point:::'.$targetpoint;
-	exit;
+	//echo 'point:::'.$targetpoint;
+//	exit;
 	//$sync_log=$api_target->Create($this->apis[$connection->api_target]->write_point,$res,json_decode($connection->target_fields));
-//	$sync_log=$api_target->Create($targetpoint,$res,json_decode($connection->target_fields));
+	$sync_log=$api_target->Create($targetpoint,$res,json_decode($connection->target_fields));
 	
 	$sync_o =new stdClass;;
 	$sync_o->scheduleID=-1;
